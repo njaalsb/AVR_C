cf5adf16fc58af5e49cd2a80f772e2af431bc847
njaalsb <bruheimnjal@gmail.com>
2025-03-27 23:44:03 +0100

    Opprettet D10

---
+++ D10/ADC.c
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.c
+ * Author: bruhe
+ *
+ * Created on February 10, 2025, 3:11 PM
+ * 
+ * PINOUT:
+ * PD5 = Signal 2 inn
+ * 
+ * Kilder: 
+ * - Getting Started with Analog-to-Digital Converter (ADC). Microchip 10/2/2025
+ * - ERT 23. Carl Richard Steen Fosse, NTNU, 10/2/2025
+ */
+
+
+ #include "ADC.h"
+ 
+void ADC0_init(void)
+{
+    /* Disable digital input buffer */
+    PORTD.PIN6CTRL &= ~PORT_ISC_gm;
+    PORTD.PIN6CTRL |= PORT_ISC_INPUT_DISABLE_gc;
+    
+    /* Disable pull-up resistor */
+    PORTD.PIN6CTRL &= ~PORT_PULLUPEN_bm;
+    
+    ADC0.CTRLC = ADC_PRESC_DIV256_gc; /* CLK_PER divided by 256 */
+    ADC0.CTRLA = ADC_ENABLE_bm /* ADC Enable: enabled */
+               | ADC_RESSEL_10BIT_gc; /* 10-bit mode */
+    
+    /* Select ADC channel */
+    ADC0.MUXPOS = ADC_MUXPOS_AIN5_gc;
+    VREF.ADC0REF |= VREF_REFSEL_VDD_gc;
+    
+    ADC0.INTCTRL |= ADC_RESRDY_bm; // Aktiver interrupt
+    ADC0.CTRLA |= ADC_FREERUN_bm; // Sett freerun-modus
+}
---
+++ D10/ADC.h
@@ -0,0 +1,30 @@
+
+/* 
+ * File:   ADC.h
+ * Author: bruhe
+ *
+ * Created on March 18, 2025, 10:40 AM
+ */
+
+ #ifndef ADC_H
+ #define	ADC_H
+ 
+ #ifdef	__cplusplus
+ extern "C" {
+ #endif
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <avr/io.h>
+ #include <string.h>
+ 
+
+
+ void ADC0_init(void);
+ //uint16_t ADC0_read(void);
+
+ #ifdef	__cplusplus
+ }
+ #endif
+
+ #endif	/* ADC_H */
